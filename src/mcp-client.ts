1d138dac837c3267aad46e9604b4dd0d8b29f0ae
Shinya Fujino <shf0811@gmail.com>
2025-03-29 21:48:48 +0900

    ping

--- src/mcp-client.ts
+++ src/mcp-client.ts
@@ -1,11 +1,23 @@
+import readline from "node:readline/promises";
 import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
-import type { Tool } from "@modelcontextprotocol/sdk/types.js";
+
+const Method = {
+	ping: "ping",
+	prompts: {
+		list: "prompts/list",
+	},
+	resources: {
+		list: "resources/list",
+	},
+	tools: {
+		list: "tools/list",
+	},
+} as const;
 
 class MCPClient {
 	private mcp: Client;
 	private transport: StdioClientTransport | null = null;
-	private tools: Tool[] = [];
 
 	constructor() {
 		this.mcp = new Client({ name: "mcpsh", version: "0.0.1" });
@@ -28,25 +40,63 @@ class MCPClient {
 				command,
 				args: [serverScriptPath],
 			});
-			this.mcp.connect(this.transport);
+			await this.mcp.connect(this.transport);
 
-			this.tools = (await this.mcp.listTools()).tools;
-			console.log(
-				"Connected to server with tools:",
-				this.tools.map(({ name }) => name),
-			);
+			console.log("Connected to server");
+			console.log("Version:", this.mcp.getServerVersion());
+			console.log("Capabilities:", this.mcp.getServerCapabilities());
+			console.log("Instructions:", this.mcp.getInstructions());
 		} catch (error) {
 			console.log("Failed to connect to server: ", error);
 			throw error;
 		}
 	}
 
+	async processQuery(query: string) {
+		switch (query.toLowerCase()) {
+			case Method.ping: {
+				this.printRequest({ method: Method.ping });
+				const response = await this.mcp.ping();
+				this.printResponse(response);
+				break;
+			}
+			default:
+				console.error(`Unknown method: ${query}`);
+				return;
+		}
+	}
+
 	async loop() {
-		console.log("> TODO: loop");
+		const rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		});
+
+		try {
+			while (true) {
+				const query = await rl.question("> ");
+
+				if (query.toLowerCase() === "quit" || query.toLowerCase() === "q") {
+					break;
+				}
+
+				await this.processQuery(query);
+			}
+		} finally {
+			rl.close();
+		}
 	}
 
 	async cleanup() {
 		await this.mcp.close();
+	}
+
+	printRequest(request: Record<string, unknown>) {
+		console.log("Request:", JSON.stringify(request, null, 2));
+	}
+
+	printResponse(response: Record<string, unknown>) {
+		console.log("Response:", JSON.stringify(response, null, 2));
 	}
 }
 
